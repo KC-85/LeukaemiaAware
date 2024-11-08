689c44a4198f1d52ed9bf158a8fa3b460f87c45e
Kristian Cross <etamin1985@yahoo.com>
2024-11-08 20:08:57 +0000

    Add cofigurations to connect to server

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// vue.config.js
+module.exports = {
+  devServer: {
+    allowedHosts: "all", // Allow all hostnames
+    host: "0.0.0.0", // Accept connections from any IP
+    port: 8080, // Port for your dev server
+  },
+};
