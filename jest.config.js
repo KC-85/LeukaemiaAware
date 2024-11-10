2cb8f316f2e4c594ed5f6773e4ba11d2ca68484b
Kristian Cross <etamin1985@yahoo.com>
2024-11-10 17:43:07 +0000

    Test results & update JEST transform configs

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-    testEnvironment: "jsdom",
-    transform: {
-      "^.+\\.vue$": "vue-jest", // Handles .vue files
-      "^.+\\.js$": "babel-jest", // Handles .js files
-    },
-    moduleFileExtensions: ["js", "json", "vue"],
-    transformIgnorePatterns: [
-      "/node_modules/", // Avoid transforming dependencies
-    ],
-    setupFilesAfterEnv: ["@testing-library/jest-dom"], // Extend Jest with custom matchers
-    collectCoverage: true, // Generate coverage reports
-    collectCoverageFrom: [
-      "src/**/*.{js,vue}", // Collect coverage from all source files
-      "!src/main.js", // Exclude entry point
-      "!**/node_modules/**",
-    ],
-  };
-  
+  testEnvironment: "jsdom", // Simulates a browser-like environment
+  transform: {
+    "^.+\\.vue$": "vue-jest", // Handles Vue single-file components
+    "^.+\\.js$": "babel-jest", // Handles JavaScript files
+  },
+  moduleFileExtensions: ["js", "json", "vue"], // Recognize these extensions
+  transformIgnorePatterns: [
+    "/node_modules/", // Don't transform node_modules by default
+  ],
+  setupFilesAfterEnv: ["@testing-library/jest-dom"], // Custom matchers for DOM
+  collectCoverage: true, // Generate coverage reports
+  collectCoverageFrom: [
+    "src/**/*.{js,vue}", // Include JS and Vue files in coverage
+    "!src/main.js", // Exclude the entry point file
+    "!**/node_modules/**",
+  ],
+  extensionsToTreatAsEsm: [".vue"], // Treat Vue files as ES modules
+};
